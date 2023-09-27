c1f1a151b8ed3c2adb0f23975b4992f7d7658763
danthecm <etchiedaniel1@gmail.com>
2023-09-27 01:28:39 +0100

    0x00: task 10: initial

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (var idx in array) {
-      var value = array[idx];
-      array[idx] = appendString + value;
-    }
-  
-    return array;
+  const newArray = [];
+  for (const value of array) {
+    newArray[array.indexOf(value)] = appendString + value;
+  }
+
+  return newArray;
 }
