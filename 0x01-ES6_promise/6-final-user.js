629afd8e4bda718ea3ded297a84f5cc9bc6648ca
danthecm <etchiedaniel1@gmail.com>
2023-09-28 01:47:56 +0100

    0x01: task 6 fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,20 @@
-import { uploadPhoto } from './utils';
+import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
-    uploadPhoto(),
     signUpUser(firstName, lastName),
-  ]).then((result) => console.log(result));
+    uploadPhoto(fileName),
+  ]).then((value) => {
+    const result = [];
+    value.forEach((res) => {
+      if (res.status !== 'fulfilled') {
+        const { status, reason } = res;
+        result.push({ status, value: reason });
+        return;
+      }
+      result.push(res);
+    });
+    return result;
+  });
 }
