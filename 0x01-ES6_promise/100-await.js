f63a03da2ac237e9bb562dfbb1be88f1a244e265
danthecm <etchiedaniel1@gmail.com>
2023-09-28 10:40:25 +0100

    0x01: task 10 fix

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -5,15 +5,11 @@ export default async function asyncUploadUser() {
   try {
     const uploadResult = await uploadPhoto();
     result.photo = uploadResult;
-  } catch (e) {
-    result.photo = null;
-  }
-
-  try {
     const createUserResult = await createUser();
     result.user = createUserResult;
   } catch (e) {
     result.user = null;
+    result.photo = null;
   }
   return result;
 }
