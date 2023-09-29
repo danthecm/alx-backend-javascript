3f8c257a82b3f920356e0d8d4791aaea0d23340e
danthecm <etchiedaniel1@gmail.com>
2023-09-29 02:05:24 +0100

    0x02: task 5 fix

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,12 @@
 export default class Building {
   constructor(sqft) {
+    if (new.target !== Building) {
+      if (this.evacuationWarningMessage === undefined) {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
     this.sqft = sqft;
   }
 
@@ -10,10 +17,15 @@ export default class Building {
   set sqft(value) {
     this._sqft = value;
   }
+}
 
-  static evacuationWarningMessage() {
-    throw new Error(
-      'Class extending Building must override evacuationWarningMessage',
-    );
-  }
+const b = new Building(100);
+console.log(b);
+
+class TestBuilding extends Building {}
+
+try {
+  TestBuilding(200);
+} catch (err) {
+  console.log(err);
 }
