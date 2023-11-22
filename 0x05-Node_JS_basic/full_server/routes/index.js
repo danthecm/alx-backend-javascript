308f5f6bce99ae598d7c6b5fa8e012cb8c65ea00
rrsoft <rrsoft@rrsofts-MacBook-Pro.local>
2023-11-22 02:48:55 +0100

    0x05:

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,15 @@
+/**
+ * Indicates specific routes and handlers
+ * @param {Express} app - Simple HTTP app
+ */
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
+const mapRoutes = (app) => {
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
+
+export default mapRoutes;
+module.exports = mapRoutes;
